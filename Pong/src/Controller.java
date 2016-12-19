780f33c56d7d5a70f25821b2b906e13eb6ab6ca7
Tobiasz Czernigowski <t.czernigowski@gmail.com>
2016-12-19 21:42:19 +0100

    Initial commit for pong game implemented in Java.

---
+++ Pong/src/Controller.java
@@ -0,0 +1,29 @@
+import java.awt.EventQueue;
+
+public class Controller {
+
+	private Ball ball;
+	private PongRacket player1Racket;
+	private PongRacket player2Racket;
+	private static MainWindow frame;
+	private int player1Score;
+	private int player2Score;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new MainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
---
+++ Pong/src/MainWindow.java
@@ -0,0 +1,57 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class MainWindow extends JFrame {
+	private String score;
+	private JPanel contentPane;
+	private JLabel scoreLabel;
+	private JPanel gameBoard;
+	private JButton btnNewButton;
+
+
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 598, 414);
+		setSize(600, 600);
+		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		score = "Score 0 - 0";
+		
+		scoreLabel = new JLabel(score);
+		scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		scoreLabel.setFont(new Font("Lucida Sans", Font.PLAIN, 30));
+		contentPane.add(scoreLabel, BorderLayout.NORTH);
+		
+		gameBoard = new JPanel();
+		contentPane.add(gameBoard, BorderLayout.CENTER);
+		
+		btnNewButton = new JButton("Reset game");
+		btnNewButton.setFont(new Font("Lucida Console", Font.PLAIN, 14));
+		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Sets the text displayed on the score board.
+	 * @param score1 player 1 new score
+	 * @param score2 player 2 new score
+	 */
+	public void setScore(int score1, int score2){
+		this.score = "Score " + score1 + " - " + score2;
+		this.scoreLabel.setText(score);
+	}
+}
